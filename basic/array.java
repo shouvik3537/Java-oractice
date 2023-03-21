215231159bb5fc908daeb8dda9f04503b13b2c13
shouvik3537 <shouvik3537@gmail.com>
2023-03-21 11:18:23 +0530

    new files

--- basic/array.java
+++ basic/array.java
@@ -1,20 +1,10 @@
 import java.util.Scanner;
-
-import javax.swing.event.SwingPropertyChangeSupport;
 public class array {
     public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     System.out.println("enter the value");
     int n = in.nextInt();
-     printall(n);
+     
 
 }
-
-
-static void printall(int a){
-    for(int i=0; i<a;i++){
-        System.out.println("count");
-    }
-}
-
 }
---
+++ linear search/Linearsearch.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Linearsearch {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        int[] numbs = new int[6];
+        for(int i=0;i<6;i++){
+            numbs[i] = in.nextInt();
+        }
+        in.close();
+        for(int i=0;i<6;i++){
+            System.out.println("numbs[i]");
+        }
+    }
+
+}
